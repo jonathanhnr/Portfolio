e0ee5a905cd162eac213e3e3529b70d8a3eed899
jonathan henrique <henriquejonathan500@gmail.com>
2023-04-26 17:03:51 -0300

    add config github pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"https://github.com/jonathanhnr.github.io/Portfolio/"
+  base:"/Portfolio/"
   ,
   server: {
     port: 3000,
