39b671911b8629f760341e18ed6905763f1f0f94
jonathan henrique <henriquejonathan500@gmail.com>
2023-04-28 18:29:11 -0300

    bugs animations

--- src/Section/Projects/Projects.jsx
+++ src/Section/Projects/Projects.jsx
@@ -8,11 +8,6 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 const Projects = () => {
-  useEffect(() => {
-    AOS.init({
-      duration: 2000,
-    });
-  }, []);
   return (
     <SectionProjects id={'projects'}>
       <div className={'container'}>
